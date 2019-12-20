90232c081453254808e3ad3a88eccf8a4d2968fa
Misbah ansori <misbah.ansori24@gmail.com>
2019-12-20 22:37:37 +0700

    jadi

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    fontFamily : {
+      'sans' : ['Work Sans'],
+    },
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '128' : '32rem',
+      },
+      borderRadius : {
+        'xl' : '.8rem',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
